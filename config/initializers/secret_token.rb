a68557b2112576b9a73c8693a58a5e7f337d2b26
John Hinrichs <jmhinric@gmail.com>
2014-02-23 17:23:24 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Minesweeper::Application.config.secret_key_base = 'f978d4954954549f9b96cc41258b8474cd8adf05c4c9a1c2c30d84f8e5d959621f7eaaeb4bae46bbbee13687a0391049d32e3ac6a8a8793a4e86f0b81e760c6a'
